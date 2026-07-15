dbdd0b34afde1bd668002537a75cdd6465f0f134
eugene234466 <eugeneyarney5@gmail.com>
2026-07-15 22:41:09 +0000

    Created dart files

---
+++ lib/models/check_in.dart
@@ -0,0 +1,26 @@
+class CheckIn {
+  final int? id;
+  final int? habitId;
+  final String status;
+  final DateTime createdAt;
+
+  CheckIn({this.id, this.habitId, required this.status, required this.createdAt});
+
+  Map<String, dynamic> toMap(){
+    return{
+      'id': id,
+      'habitId': habitId,
+      'status': status,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+  factory CheckIn.fromMap(Map<String, dynamic> map){
+    return CheckIn(
+      id: map['id'] as int?,
+      habitId: map['habitId'] as int?,
+      status: map['status'] as String,
+      createdAt: DateTime.parse(map['createdAt'] as String),
+    );
+  }
+
+}
