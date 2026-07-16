4f04d4308176f560044c4b84afbb56bc49763349
eugene234466 <eugeneyarney5@gmail.com>
2026-07-16 14:17:12 +0000

    App complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class HabitTrackerApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Habit Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
